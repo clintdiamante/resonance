8269f6c4cfec981a51446466ac204790b65826d7
Clint Diamante <clintdiamante@gmail.com>
2026-02-12 09:50:39 +0800

    create initial layout backbone with header and footer

---
+++ client/src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+
+      {/* Header */}
+      <header className="header">
+        
+      </header>
+
+      {/* Main Content */}
+      <main className="main">
+
+      </main>
+      
+      {/* Footer */}
+      <footer className="footer">
+
+      </footer>
+
+    </div>
+  );
+}
+
+export default App
