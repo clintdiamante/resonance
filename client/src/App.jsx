99959ff469076e02fadc74d5b39637b3d5154eb0
Clint Diamante <clintdiamante@gmail.com>
2026-02-12 10:25:47 +0800

    build minimalist header with nav links icons and search layer structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,35 @@ function App() {
 
       {/* Header */}
       <header className="header">
-        
+        <div className="header-inner">
+          
+          {/* Left - Brand */}
+          <div className="brand">
+            <h1 className="logo">OMSIM</h1>
+          </div>
+
+          {/* Right - Navigation */}
+          <nav className="nav">
+            <a href="#">Home</a>
+            <a href="#">Shop</a>
+            <a href="#">Contact</a>
+
+            <button className="icon account">Account</button>
+            <button className="icon search">Search</button>
+            <button className="icon cart">Cart</button>
+          </nav>
+
+          {/* Search Bar */}
+          <div className="search-layer">
+            <input
+              type="search"
+              placeholder="Search Products..."
+              className="search-input"
+            />
+
+            <button className="search-close">X</button>
+          </div>
+        </div>
       </header>
 
       {/* Main Content */}
